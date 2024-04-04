4b10f903c663eeee417de384ddad61c561bf4235
Michael JeanBaptiste <mikejean4567@gmail.com>
2024-04-03 21:59:53 -0400

    finished RightTriangle

--- hello/src/RightTriangle.java
+++ hello/src/RightTriangle.java
@@ -2,10 +2,15 @@
 public class RightTriangle {
 
     public static void main(String[] args) {
-        int a = (int)Math.pow(args[0], 2);
-        int b = (int)Math.pow(args[1], 2);
-        int c = (int)Math.pow(args[2], 2);
-        if(a + b != c){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        int aSquared = (int)Math.pow(a, 2);
+        int bSquared = (int)Math.pow(b, 2);
+        int cSquared = (int)Math.pow(c, 2);
+
+        if(aSquared + bSquared != cSquared){
             System.out.println("false");
         }
         else{
